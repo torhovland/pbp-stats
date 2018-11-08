c31acff545e856c9df761f8a1833f73daf3134ac
Tor Hovland <tor.hovland@bekk.no>
2018-11-08 19:45:07 +0100

    Reading the other table columns as well.

--- participant-downloader/src/main.rs
+++ participant-downloader/src/main.rs
@@ -10,6 +10,10 @@ fn main() {
     println!("Participant");
 }
 
+fn next_cell(row_iter: &mut scraper::element_ref::Select) -> Option<String> {
+    row_iter.next().map(|td| td.inner_html())
+}
+
 fn download_participants() -> Result<(), Error> {
     let html = reqwest::get("http://www.paris-brest-paris.org?lang=en&cat=presentation&page=resultats_2015")?
         .text()?;
@@ -19,11 +23,20 @@ fn download_participants() -> Result<(), Error> {
     let td_selector = Selector::parse("td").unwrap();
 
     for tr in document.select(&tr_selector) {
-        let option_time = tr.select(&td_selector).next().map(|td| td.inner_html());
+        let mut iter = tr.select(&td_selector);
+        let option_time = next_cell(&mut iter);
+        let option_id = next_cell(&mut iter);
+        let option_last_name = next_cell(&mut iter);
+        let option_first_name = next_cell(&mut iter);
+        let option_country = next_cell(&mut iter);
+        let option_gender = next_cell(&mut iter);
+        let option_mach = next_cell(&mut iter);
+        let option_club = next_cell(&mut iter);
 
-        match option_time {
-            Some(time) => println!("{}", time),
-            None => println!("Skipping row")
+        match (option_time, option_id, option_last_name, option_first_name, option_country, option_gender, option_mach, option_club) {
+            (Some(time), Some(id), Some(last_name), Some(first_name), Some(country), Some(gender), Some(mach), Some(club)) => 
+                println!("{} - {} - {} - {} - {} - {} - {} - {}", time, id, last_name, first_name, country, gender, mach, club),
+            _ => println!("Skipping row")
         };        
     }
 
