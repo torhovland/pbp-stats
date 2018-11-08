5b4a5496c455892405e86f1ec8abc73b36697a26
Tor Hovland <tor.hovland@bekk.no>
2018-11-08 18:55:53 +0100

    Reading the HTML table using scraper.

--- participant-downloader/src/main.rs
+++ participant-downloader/src/main.rs
@@ -1,10 +1,30 @@
+extern crate failure;
 extern crate reqwest;
+extern crate scraper;
+
+use failure::Error;
+use scraper::{Html, Selector};
 
 fn main() {
-    let participant = download_participants().unwrap();
-    println!("Participant: {}", participant);
+    download_participants().expect("Failed to download the participant list.");
+    println!("Participant");
 }
 
-fn download_participants() -> Result<String, reqwest::Error> {
-    reqwest::get("http://www.paris-brest-paris.org?lang=en&cat=presentation&page=resultats_2015")?.text()
+fn download_participants() -> Result<(), Error> {
+    let html = reqwest::get("http://www.paris-brest-paris.org?lang=en&cat=presentation&page=resultats_2015")?
+        .text()?;
+
+    let document = Html::parse_document(&html);
+    let tr_selector = Selector::parse("tr").unwrap();
+    let td_selector = Selector::parse("td").unwrap();
+
+    for tr in document.select(&tr_selector) {
+        println!("tr");
+
+        for td in tr.select(&td_selector) {
+            println!("{}", td.inner_html());        
+        }
+    }
+
+    Ok(())
 }
