155856ff674c146b6b92a000daa993d6bc3f8579
Tor Hovland <tor.hovland@bekk.no>
2018-11-13 21:21:08 +0100

    Parsing participant numbers.

--- participant-downloader/src/main.rs
+++ participant-downloader/src/main.rs
@@ -5,9 +5,54 @@ extern crate scraper;
 use failure::Error;
 use scraper::{Html, Selector};
 
+mod participant_id
+{
+    use std::fmt;
+    
+    struct ParticipantIdNumber(u16);
+
+    impl ParticipantIdNumber {
+        fn new(i: u16) -> ParticipantIdNumber {
+            assert!(i >= 1 && i < 1000);
+            ParticipantIdNumber(i)
+        }
+    }
+
+    impl fmt::Debug for ParticipantIdNumber {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "{}", &self.0) }
+    }
+
+    #[derive(Debug)]
+    pub struct Id {
+        letter: char,
+        number: ParticipantIdNumber,
+    }
+
+    pub fn parse_id(s: String) -> Option<Id> {
+        match s.len() {
+            4 => Some(Id { 
+                letter: s.chars().next().unwrap(), 
+                number: ParticipantIdNumber::new(s[1..].parse().unwrap()) 
+            }),
+            _ => None
+        }    
+    }
+}
+
+#[derive(Debug)]
+struct Participant {
+    time: String,
+    id: participant_id::Id,
+    last_name: String,
+    first_name: String,
+    country: String,
+    gender: String,
+    mach: String,
+    club: String,
+}
+
 fn main() {
     download_participants().expect("Failed to download the participant list.");
-    println!("Participant");
 }
 
 fn next_cell(row_iter: &mut scraper::element_ref::Select) -> Option<String> {
@@ -24,20 +69,47 @@ fn download_participants() -> Result<(), Error> {
 
     for tr in document.select(&tr_selector) {
         let mut iter = tr.select(&td_selector);
-        let option_time = next_cell(&mut iter);
-        let option_id = next_cell(&mut iter);
-        let option_last_name = next_cell(&mut iter);
-        let option_first_name = next_cell(&mut iter);
-        let option_country = next_cell(&mut iter);
-        let option_gender = next_cell(&mut iter);
-        let option_mach = next_cell(&mut iter);
-        let option_club = next_cell(&mut iter);
 
-        match (option_time, option_id, option_last_name, option_first_name, option_country, option_gender, option_mach, option_club) {
-            (Some(time), Some(id), Some(last_name), Some(first_name), Some(country), Some(gender), Some(mach), Some(club)) => 
-                println!("{} - {} - {} - {} - {} - {} - {} - {}", time, id, last_name, first_name, country, gender, mach, club),
-            _ => println!("Skipping row")
-        };        
+        let participant = match (
+            next_cell(&mut iter), 
+            next_cell(&mut iter), 
+            next_cell(&mut iter), 
+            next_cell(&mut iter), 
+            next_cell(&mut iter), 
+            next_cell(&mut iter), 
+            next_cell(&mut iter), 
+            next_cell(&mut iter)) {
+            (
+                Some(time), 
+                Some(id_string),
+                Some(last_name), 
+                Some(first_name), 
+                Some(country), 
+                Some(gender), 
+                Some(mach), 
+                Some(club)
+            ) => 
+                match participant_id::parse_id(id_string) {
+                    Some(id) => 
+                        Some(Participant {
+                            time,
+                            id,
+                            last_name,
+                            first_name,
+                            country,
+                            gender,
+                            mach,
+                            club
+                        }),
+                    _ => None
+                },
+            _ => None
+        };
+
+        match participant { 
+            Some(p) => println!("{:?}", p), 
+            _ => println!("No participant")
+        };
     }
 
     Ok(())
